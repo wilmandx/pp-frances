555d66423a13900461edb42bd14216bd5a2841c9
Wilman Tello Borja <xtellwi@ville.montreal.qc.ca>
2017-08-25 17:29:45 -0400

    simple 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     children: []
-  }
+  },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
